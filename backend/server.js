67007bc0e24216b967cbe76bf311388ea0569502
MD.Anis Molla <41534160+anichu@users.noreply.github.com>
2022-03-10 01:53:40 +0600

    completed the work of erroHandler in servejs file

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const colors = require("colors");
+const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 require("dotenv").config({ path: "./config.env" });
 
 // Connect to MongoDB
 connectDB();
+
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
 app.use("/me", (req, res, next) => {
 	res.send("anis molla");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () =>
 	console.log(`Server running on port ${PORT}🚀🚀🚀🚀`.underline.cyan.bold)
